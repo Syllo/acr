cabd5ad28763356e0af56876485d6fb631a4974c
Maxime Schmitt <maxime.schmitt91@gmail.com>
2016-02-03 11:35:00 +0100

    Starting grammar

---
+++ cmake/modules/FindCLAN.cmake
@@ -0,0 +1,38 @@
+# Version 1.0
+# Public Domain
+# Written by Maxime SCHMITT <maxime.schmitt@etu.unistra.fr>
+
+#/////////////////////////////////////////////////////////////////////////////#
+#                                                                             #
+# Search for the Chunky Loop Analyser Library on the system                   #
+# Call with find_package(CLAN)                                                #
+# The module defines:                                                         #
+#   - CLAN_FOUND        - If CLAN was found                                   #
+#   - CLAN_INCLUDE_DIRS - the CLAN include directories                        #
+#   - CLAN_LIBRARIES    - the CLAN library directories                        #
+#   - CLAN_EXECUTABLE   - the CLAN executable                                 #
+#                                                                             #
+#/////////////////////////////////////////////////////////////////////////////#
+
+include(LibFindMacros)
+
+# Get hints about paths
+libfind_pkg_check_modules(CLAN_PKGCONF clan)
+
+# Headers
+find_path(CLAN_INCLUDE_DIR
+  NAMES "clan/clan.h"
+  PATHS ${CLAN_PKGCONF_INCLUDE_DIRS})
+
+# Library
+find_library(CLAN_LIBRARY
+  NAMES clan
+  PATHS ${CLAN_PKGCONF_LIBRARY_DIRS})
+
+# Executable
+find_program(CLAN_EXECUTABLE
+  NAMES clan)
+
+set(CLAN_PROCESS_LIBS CLAN_LIBRARY)
+set(CLAN_PROCESS_INCLUDES CLAN_INCLUDE_DIR)
+libfind_process(CLAN)
---
+++ cmake/modules/FindOSL.cmake
@@ -0,0 +1,33 @@
+# Version 1.0
+# Public Domain
+# Written by Maxime SCHMITT <maxime.schmitt@etu.unistra.fr>
+
+#/////////////////////////////////////////////////////////////////////////////#
+#                                                                             #
+# Search for the OpenScop Library on the system                               #
+# Call with find_package(OSL)                                                 #
+# The module defines:                                                         #
+#   - OSL_FOUND        - If OSL was found                                     #
+#   - OSL_INCLUDE_DIRS - the OSL include directories                          #
+#   - OSL_LIBRARIES    - the OSL library directories                          #
+#                                                                             #
+#/////////////////////////////////////////////////////////////////////////////#
+
+include(LibFindMacros)
+
+# Get hints about paths
+libfind_pkg_check_modules(OSL_PKGCONF osl)
+
+# Headers
+find_path(OSL_INCLUDE_DIR
+  NAMES "osl/osl.h"
+  PATHS ${OSL_PKGCONF_INCLUDE_DIRS})
+
+# Library
+find_library(OSL_LIBRARY
+  NAMES osl
+  PATHS ${OSL_PKGCONF_LIBRARY_DIRS})
+
+set(OSL_PROCESS_LIBS OSL_LIBRARY)
+set(OSL_PROCESS_INCLUDES OSL_INCLUDE_DIR)
+libfind_process(OSL)
