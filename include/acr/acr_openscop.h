579c4b09411a3eb54d71cdac082d3543998190fa
Maxime Schmitt <maxime.schmitt91@gmail.com>
2016-02-16 17:36:48 +0100

    openscope

---
+++ include/acr/acr_openscop.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016 Maxime Schmitt
+ *
+ * ACR is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __ACR_OPENSCOP_H
+#define __ACR_OPENSCOP_H
+
+#include "acr/pragma_struct.h"
+
+#include <osl/scop.h>
+#include <clan/scop.h>
+
+osl_scop_p acr_extract_scop_in_compute_node(const acr_compute_node node,
+                                            FILE* input_file,
+                                            char* name_input_file);
+
+#endif // __ACR_OPENSCOP_H
