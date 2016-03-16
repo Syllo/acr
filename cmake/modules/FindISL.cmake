59a7bbe93468ec27c9d0f08d9301cac35d1f51f1
Maxime Schmitt <maxime.schmitt91@gmail.com>
2016-03-16 22:12:58 +0100

    Cmake

--- cmake/modules/FindISL.cmake
+++ cmake/modules/FindISL.cmake
@@ -10,6 +10,7 @@
 #   - ISL_FOUND        - If ISL was found                                     #
 #   - ISL_INCLUDE_DIRS - the ISL include directories                          #
 #   - ISL_LIBRARIES    - the ISL library directories                          #
+#   - ISL_VERSION      - the ISL version                                      #
 #                                                                             #
 #/////////////////////////////////////////////////////////////////////////////#
 
@@ -27,6 +28,50 @@ find_path(ISL_INCLUDE_DIR
 find_library(ISL_LIBRARY
   NAMES isl
   PATHS ${ISL_PKGCONF_LIBRARY_DIRS})
+
+# Version
+set(ISL_GET_VERSION_C
+  "
+  #include <stdlib.h>
+  #include <stdio.h>
+  #include <isl/version.h>
+
+  int main(int argc, char **argv) {
+    fprintf(stdout, \"%s\", isl_version())\;
+    return EXIT_SUCCESS\;
+  }
+  ")
+
+file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.c"
+  ${ISL_GET_VERSION_C})
+
+try_run(ISL_EXIT_CODE ISL_COMPILED
+  ${CMAKE_BINARY_DIR}
+  ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.c
+  COMPILE_DEFINITIONS "-DINCLUDE_DIRECTORIES:STRING=${ISL_INCLUDE_DIR}"
+  LINK_LIBRARIES ${ISL_LIBRARY}
+  RUN_OUTPUT_VARIABLE ISL_GET_VERSION_RUN_OUTPUT
+  COMPILE_OUTPUT_VARIABLE ISL_COMPILE_RETURN)
+
+if(ISL_COMPILED AND ISL_EXIT_CODE EQUAL 0)
+  string(REGEX REPLACE ".*-([0-9]+[.][0-9]+[.][0-9]+)-.*" "\\1"
+    ISL_VERSION_TEMP
+    "${ISL_GET_VERSION_RUN_OUTPUT}")
+  if(ISL_VERSION_TEMP STREQUAL "${ISL_GET_VERSION_RUN_OUTPUT}")
+    string(REGEX REPLACE ".*-([0-9]+[.][0-9]+)-.*" "\\1"
+      ISL_VERSION_TEMP
+      "${ISL_GET_VERSION_RUN_OUTPUT}")
+    if(ISL_VERSION_TEMP STREQUAL "${ISL_GET_VERSION_RUN_OUTPUT}")
+      message(AUTHOR_WARNING "Unable to find ISL version in string: ${ISL_GET_VERSION_RUN_OUTPUT}")
+    else()
+      set(ISL_VERSION ${ISL_VERSION_TEMP})
+    endif()
+  else()
+    set(ISL_VERSION ${ISL_VERSION_TEMP})
+  endif()
+else()
+  message(AUTHOR_WARNING "Unable to run program to get ISL version")
+endif()
 
 set(ISL_PROCESS_LIBS ISL_LIBRARY)
 set(ISL_PROCESS_INCLUDES ISL_INCLUDE_DIR)
